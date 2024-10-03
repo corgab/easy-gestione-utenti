cecdbeec698c51172d54fe28ba184015845b2e77
gab <corbanigabriele@gmail.com>
2024-10-03 13:01:47 +0200

    ADD: variabili d'ambiente

--- models/db.js
+++ models/db.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
+require('dotenv').config() // Carico le variabili dall'env
 
 // Connessione mysql
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', 
-  database: 'user_management',
-  port: 8889
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 // Gestione errori
